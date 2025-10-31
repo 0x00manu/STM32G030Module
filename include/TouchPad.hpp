687df1158a53fd982e7ef8d1c0e3209570fa5092
Manu Prakash <tomanuprakash@rediffmail.com>
2025-10-31 18:14:53 +0530

    adding IRremote lib and doc

---
+++ include/TouchPad.hpp
@@ -0,0 +1,13 @@
+#ifndef TOUCHPAD_HPP
+#define TOUCHPAD_HPP
+//#include "Remote.hpp"
+/// @brief Contains various touchSwitch(s)
+class TouchPad
+{
+private:
+public:
+    TouchPad(
+    );
+    ~TouchPad();
+};
+#endif
